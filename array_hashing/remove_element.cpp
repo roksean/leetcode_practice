4c9ca57d86f2b166732edfed73475138cca4497e
seanOkeefe2003 <seanokeefe03@gmail.com>
2025-06-26 18:35:55 -0400

    remove element

---
+++ array_hashing/remove_element.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        nums.erase(remove(nums.begin(), nums.end(), val), nums.end());
+        return nums.size();
+    }
+};
